6349c8b8ed7c37854da66b847a316c3c088ff9f0
Julien Eperon <julien.eperon@gmail.com>
2017-10-15 21:13:56 +0200

    Initial spring boot project

---
+++ src/main/java/ch/urbanfox/freqtrade/FreqtradeApplication.java
@@ -0,0 +1,12 @@
+package ch.urbanfox.freqtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreqtradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreqtradeApplication.class, args);
+	}
+}
