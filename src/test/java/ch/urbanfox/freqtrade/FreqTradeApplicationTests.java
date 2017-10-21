e0ce58c0ce4839534c7d021d3badcfdcff870b66
Julien Eperon <julien.eperon@gmail.com>
2017-10-21 20:29:11 +0200

    Forcing test profile in tests

--- src/test/java/ch/urbanfox/freqtrade/FreqTradeApplicationTests.java
+++ src/test/java/ch/urbanfox/freqtrade/FreqTradeApplicationTests.java
@@ -3,14 +3,16 @@ package ch.urbanfox.freqtrade;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles(profiles = "test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FreqTradeApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
